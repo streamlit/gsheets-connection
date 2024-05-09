a5762d2d7a111e13588e6e1eb698ceacad2478d4
Sanaz Shahraeini <79825630+sanaz-shahraeini@users.noreply.github.com>
2024-05-09 11:53:29 +0330

    Update Public_Sheet_Example.py

--- Public_Sheet_Example.py
+++ Public_Sheet_Example.py
@@ -15,25 +15,3 @@ with st.echo():
 
     df = conn.read(spreadsheet=url, usecols=[0, 1])
     st.dataframe(df)
-
-st.write("#### 2. Query public Google Worksheet using SQL")
-st.info(
-    "Mutation SQL queries are in-memory only and do not results in the Worksheet update.",
-)
-st.warning(
-    """You can query only one Worksheet in provided public Spreadsheet,
-        use Worksheet name as target in from SQL queries.
-        The worksheet, which you query is defined by GID query parameter or GID parameters to query method.""",
-    icon="⚠️",
-)
-
-
-with st.echo():
-    import streamlit as st
-
-    from streamlit_gsheets import GSheetsConnection
-
-    conn = st.connection("gsheets", type=GSheetsConnection)
-
-    df = conn.query('select births from "Example 2" limit 10', spreadsheet=url)
-    st.dataframe(df)
